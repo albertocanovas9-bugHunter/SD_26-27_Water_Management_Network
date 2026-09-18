b17045df9e2a189cb19a2f7e80bb354330a79409
Alberto Vicente Canovas <avc86@gcloud.ua.es>
2026-09-18 19:50:18 +0200

    Repo inicial

---
+++ src/holaMundo/holaMundo.java
@@ -0,0 +1,9 @@
+package holaMundo;
+
+public class holaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("¡Hola Mundo! Entorno de Eclipse y Java 17 configurado correctamente.");
+	}
+
+}
